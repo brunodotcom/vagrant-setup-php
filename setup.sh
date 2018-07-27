7fd29409ddfa97d725f1cb777eff99532dbd5d63
Bruno de Souza Rocha <brunodotcom@outlook.com>
2018-07-27 11:51:47 -0300

    Atualizando PHP para 7.2 e MySQL para 8.0.1
    Adicionando configurações para instalar o MySQL 8.0.1 e PHP 7.2

--- setup.sh
+++ setup.sh
@@ -9,8 +9,9 @@ echo "--- Definindo Senha padrao para o MySQL e suas ferramentas ---"
 
 DEFAULTPASS="vagrant"
 sudo debconf-set-selections <<EOF
-mysql-server	mysql-server/root_password password $DEFAULTPASS
-mysql-server	mysql-server/root_password_again password $DEFAULTPASS
+mysql-apt-config mysql-apt-config/select-server select mysql-8.0
+mysql-community-server mysql-community-server/root-pass password $DEFAULTPASS
+mysql-community-server mysql-community-server/re-root-pass password $DEFAULTPASS
 dbconfig-common	dbconfig-common/mysql/app-pass password $DEFAULTPASS
 dbconfig-common	dbconfig-common/mysql/admin-pass password $DEFAULTPASS
 dbconfig-common	dbconfig-common/password-confirm password $DEFAULTPASS
@@ -30,19 +31,24 @@ sudo apt-get install software-properties-common vim curl python-software-propert
 echo "--- Adicionando repositorio do pacote PHP ---"
 sudo add-apt-repository ppa:ondrej/php
 
+echo "--- Disponibilizando o pacote do Mysql 8 ---"
+wget --user-agent="Mozilla" -O /tmp/mysql-apt-config_0.8.10-1_all.deb https://dev.mysql.com/get/mysql-apt-config_0.8.10-1_all.deb;
+export DEBIAN_FRONTEND="noninteractive";
+sudo -E dpkg -i /tmp/mysql-apt-config_0.8.10-1_all.deb;
+
 echo "--- Atualizando lista de pacotes ---"
 sudo apt-get update
 
 echo "--- Instalando MySQL, Phpmyadmin e alguns outros modulos ---"
-sudo apt-get install mysql-server-5.5 mysql-client phpmyadmin --assume-yes --force-yes
+sudo -E apt-get install mysql-server mysql-client phpmyadmin --assume-yes --force-yes
 
 echo "--- Instalando PHP, Apache e alguns modulos ---"
-sudo apt-get install php7.1 php7.1-common --assume-yes --force-yes
-sudo apt-get install php7.1-cli libapache2-mod-php7.1 php7.1-mysql php7.1-curl php-memcached php7.1-dev php7.1-mcrypt php7.1-sqlite3 php7.1-mbstring php*-mysql  php-gd php-xml php-mbstring  zip unzip --assume-yes --force-yes
+sudo apt-get install php7.2 php7.2-common --assume-yes --force-yes
+sudo apt-get install php7.2-cli libapache2-mod-php7.2 php7.2-mysql php7.2-curl php-memcached php7.2-dev php7.2-mcrypt php7.2-sqlite3 php7.2-mbstring php*-mysql  php-gd php-xml php-mbstring  zip unzip --assume-yes --force-yes
 
-echo "--- Habilitando o PHP 7.1 ---"
+echo "--- Habilitando o PHP 7.2 ---"
 sudo a2dismod php5
-sudo a2enmod php7.1
+sudo a2enmod php7.2
 
 echo "--- Habilitando mod-rewrite do Apache ---"
 sudo a2enmod rewrite
@@ -56,7 +62,7 @@ sudo mv composer.phar /usr/local/bin/composer
 
 echo "--- Instalando Banco NoSQL -> Redis <- ---" 
 sudo apt-get install redis-server --assume-yes
-sudo apt-get install php7.1-redis --assume-yes
+sudo apt-get install php7.2-redis --assume-yes
 
 # Instale apartir daqui o que você desejar 
 
